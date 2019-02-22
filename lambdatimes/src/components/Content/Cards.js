ad7349e1f439f417248c469badb605734fb21182
t-tullis <t.tullis34@gmail.com>
2019-02-22 08:40:29 -0800

    fixed cards propTypes and completed tabs.js

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -14,7 +14,7 @@ const Cards = props => {
 
 // Make sure you include prop types for all of your incoming props
 Cards.propTypes = {
-  card: PropTypes.arrayOf(PropTypes.string)
+  cards: PropTypes.arrayOf(PropTypes.object)
 }
 
 export default Cards;
